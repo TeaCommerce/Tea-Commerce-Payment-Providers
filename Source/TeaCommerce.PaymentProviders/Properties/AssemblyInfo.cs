97e2480e3457e3f37c51297a98cfc6400165c816
Anders Burla Johansen <anders@burla.dk>
2014-12-04 13:43:17 +0100

    Updated assembly versions

--- Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
+++ Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Tea Commerce Payment Providers")]
 [assembly: AssemblyCompany( "Tea Commerce" )]
-[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyCopyright( "Copyright ©  2010" )]
 [assembly: AssemblyProduct("Tea Commerce Payment Providers")]
 [assembly: Guid( "9fec840b-2f1b-44d2-8567-107e1ae3d79f" )]
 
