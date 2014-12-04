ff2c154940086bca7536468605bbc01664a3deb3
Anders Burla Johansen <anders@burla.dk>
2014-12-04 13:23:04 +0100

    Updated assembly versions

--- Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
+++ Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Tea Commerce Payment Providers")]
 [assembly: Guid( "9fec840b-2f1b-44d2-8567-107e1ae3d79f" )]
 
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "3.0.0.0" )]
+[assembly: AssemblyFileVersion( "3.0.0.0" )]
