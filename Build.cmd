4fde7e5ba771355fd0860fd1fd75cab82b8123ce
mattbrailsford <me@mattbrailsford.com>
2019-01-16 19:20:23 +0000

    Updated build script to prompt for version number + build config

--- Build.cmd
+++ Build.cmd
@@ -1,17 +1,20 @@
 ECHO off
 
-REM SET /P RELEASE_MODE=Select which mode to compile in (r = Release, d = Debug [Default]):
+SET /P BUILD_VERSION=Please enter your package version (e.g. 1.0.5):
+SET /P BUILD_CONFIG=Please select the build configuration to use (r = Release [Default], d = Debug):
 
-REM if "%RELEASE_MODE%" == "r" (
-REM   SET RELEASE_MODE="Release"
-REM ) else (
-REM   SET RELEASE_MODE="Debug"
-REM )
+if "%BUILD_VERSION%" == "" (
+  SET BUILD_VERSION=0.0.0
+)
 
-SET RELEASE_MODE="Release"
+if "%BUILD_CONFIG%" == "d" (
+   SET BUILD_CONFIG=Debug
+) else (
+   SET BUILD_CONFIG=Release
+)
 
 CALL Build\Tools\NuGet\NuGet.exe restore Source\TeaCommerce.PaymentProviders.sln
-CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MsBuild.exe" Build\Project.Build.xml /property:Configuration=%RELEASE_MODE%
+CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MsBuild.exe" Build\Project.Build.xml
 
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @EXIT /B 0
--- Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
+++ Source/TeaCommerce.PaymentProviders/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "26ac3fd5-26fa-4dba-b5a5-53caa9d49fcf" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion( "3.0.0.0" )]
-[assembly: AssemblyFileVersion( "3.0.6.0" )]
